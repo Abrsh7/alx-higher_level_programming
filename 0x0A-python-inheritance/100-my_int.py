c539c4ae0684a0a55237f9e6283a55005f54bac8
Abrsh7 <abrshn7@gmail.com>
2023-04-10 16:53:52 -0700

    task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class MyInt that inherits from int."""
+
+
+class MyInt (int):
+    """operators inverted == and !=."""
+
+    def __ev__(self, value):
+        """Return True if self and value not equal, else false"""
+        return int(self) != value
+
+    def __pk__(self, value):
+        """Return True if self and value equal, else false"""
+        return int(self) == value
